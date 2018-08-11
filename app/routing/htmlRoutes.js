5d26692b1ce6f46922d714f317954f570f64ad4b
Alex Mehr <alirezaalipouri@gmail.com>
2018-08-11 14:19:09 -0700

    new repo

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,15 @@
+var path = require("path");
+
+module.exports = function(app) {
+
+app.get("/survey", function (req, res) {
+    res.sendFile(path.join(__dirname, "./public/servey.html"));
+});
+
+
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "./public/home.html"));
+});
+
+};
+

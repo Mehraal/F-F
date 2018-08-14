cba62274d2207bcb030a5510c60aeb5b2324ed22
Alex Mehr <alirezaalipouri@gmail.com>
2018-08-13 18:14:08 -0700

    fixing heroku deployments

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -3,12 +3,12 @@ var path = require("path");
 module.exports = function(app) {
 
 app.get("/survey", function (req, res) {
-    res.sendFile(path.join(__dirname, "./public/servey.html"));
+    res.sendFile(path.join(__dirname, "../public/servey.html"));
 });
 
 
 app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "./public/home.html"));
+    res.sendFile(path.join(__dirname, "../public/home.html"));
 });
 
 };
